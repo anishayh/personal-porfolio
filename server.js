e072e69ea3e24a7397597ebf0ce87bc3593b4d13
Anisha <129502276+anishayh@users.noreply.github.com>
2024-11-02 22:46:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,11 @@
 const express = require("express");
 const path = require("path");
-const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
 
-// server used to send send emails
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-// Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, "build")));
-
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
 
 // Email configuration
 const contactEmail = nodemailer.createTransport({
@@ -33,7 +24,8 @@ contactEmail.verify((error) => {
   }
 });
 
-router.post("/contact", (req, res) => {
+// Define the /contact route before the wildcard route
+app.post("/contact", (req, res) => {
   const name = req.body.firstName + req.body.lastName;
   const email = req.body.email;
   const message = req.body.message;
@@ -56,7 +48,14 @@ router.post("/contact", (req, res) => {
   });
 });
 
-app.use("/", router);
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, "build")));
 
+// Wildcard route to serve React app for other routes
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+// Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
